0a61bbc7ae78de51a10596a64bd57e4a6ad51328
chandrakantparadkar <chandrakant.paradkar@gmail.com>
2021-06-06 20:10:04 +0530

    Log service

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', function($scope) {
+myApp.controller('mainController', function($scope,$log) {
 	$scope.name = "Chandrakant";
+	$log.info("name is :"+$scope.name);
+	$log.warn("name is :"+$scope.name);
+	$log.error("name is :"+$scope.name);
+
 });
+
