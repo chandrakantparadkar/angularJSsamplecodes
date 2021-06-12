927fae53bc1ef9e8de8d0b0ecce0d589b60403a4
chandrakantparadkar <chandrakant.paradkar@gmail.com>
2021-06-12 17:55:06 +0530

    watchers code for app.js

--- digest_watchers/app.js
+++ digest_watchers/app.js
@@ -1,6 +1,20 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ["$scope","$log",function($scope,$log) {
-
+myApp.controller('mainController', ["$scope","$log","$filter",function($scope,$log,$filter) {
+    $scope.handle = '';
+    
+    $scope.lowercasehandle = function() {
+        return $filter('lowercase')($scope.handle);
+    };
+    
+    
+    $scope.$watch('handle', function(newValue, oldValue) {
+        
+        console.info('Changed!');
+        console.log('Old:' + oldValue);
+        console.log('New:' + newValue);
+        
+    });
 
 }]);
+
